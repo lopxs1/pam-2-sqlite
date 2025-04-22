58e063552af9de9b6ef73841c48e60fa304789fc
Gustavo Lopes <164338321+lopxs1@users.noreply.github.com>
2025-04-22 08:30:26 -0300

    changes

--- components/Banco.tsx
+++ components/Banco.tsx
@@ -5,8 +5,8 @@ import * as SQLite from 'expo-sqlite';
 let db;
 
 //componente
-const Banco =()=>{
-    async function Banco(){
+const Banco = () => {
+    async function Banco() {
         db = await SQLite.openDatabaseAsync('PAM2');
         if (db) {
             console.log("Banco criado");
@@ -18,46 +18,57 @@ const Banco =()=>{
     }
 
     //criar tabela
-    async function CriarTabela() {   
+    async function CriarTabela() {
         try {
             await db.execAsync(`
                 PRAGMA journal_mode = WAL;
                 CREATE TABLE IF NOT EXISTS USUARIOS (id INTEGER PRIMARY KEY NOT NULL, NAME TEXT NOT NULL);`
             )
             console.log("tabela criada")
-        }  catch (erro) {
+        } catch (erro) {
             console.log("Erro")
         }
-       
+
 
     }
 
     async function InserirDados() {
         db = await Banco();
-        try{
+        try {
             db.execAsync(
                 ` INSERT INTO USUARIOS (NOME) VALUES ('Ricardo'), ('Zé Matraca'), ('Maria'); `
             );
             console.log("Inserido");
         }
-        catch(erro){
+        catch (erro) {
             console.log("Erro ao inserir" + erro)
         }
     }
+    async function ExibirDados() {
+        db = await Banco();
+        const allRows = await db.getAllAsync('SELECT * FROM USUARIOS');
+        for (const row of allRows) {
+            console.log(row.id, row.NAME);
+        }
+    }
 
-    return(
+    return (
         <View>
-            <Button 
+            <Button
                 title="Criar BD"
                 onPress={Banco}
             />
-            <Button 
+            <Button
                 title="Criar Tabela"
                 onPress={CriarTabela}
             />
-            <Button 
+            <Button
                 title="Inserir Dados"
                 onPress={InserirDados}
+            />
+            <Button
+                title="Exibir Dados"
+                onPress={ExibirDados}
             />
         </View>
     )
